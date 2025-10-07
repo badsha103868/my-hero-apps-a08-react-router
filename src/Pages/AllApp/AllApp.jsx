c4f79f3fcb594513d82ff6cf6f5fb3a8f234bf24
badsha103868 <badshagolder5@gmail.com>
2025-10-08 01:07:48 +0600

    first commit

---
+++ src/Pages/AllApp/AllApp.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllApp = () => {
+  return (
+    <div>
+      Ami all app page
+    </div>
+  );
+};
+
+export default AbortControllerApp;
---
+++ src/Pages/Root/Root.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../../Components/Header/Navbar';
+import Footer from '../../Components/Footer/Footer';
+
+const Root = () => {
+  return (
+    <div className='mx-w-[1200px] mx-auto'>
+      <Navbar></Navbar>
+       <Footer></Footer>
+    </div>
+  );
+};
+
+export default Root;
